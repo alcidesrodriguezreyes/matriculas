1d95fce236c6679fb98ba650f6894c80eb78ecec
Alcides <alcidesrodriguezreyes@gmail.com>
2016-04-04 20:58:40 -0500

    migration estudiante fixed

--- yii/console/migrations/m160405_010504_create_student_table.php
+++ yii/console/migrations/m160405_010504_create_student_table.php
@@ -9,13 +9,13 @@ class m160405_010504_create_student_table extends Migration
         $this->createTable('estudiante', [
             'idestudiante'        => $this->primaryKey(),
             'tipo_identificacion' => $this->integer()->notNull(),
-            'identificacion'      => $this->string()->notNull(),
+            'identificacion'      => $this->string()->notNull()->unique(),
             'nombres'             => $this->string()->notNull(),
             'apellidos'           => $this->string()->notNull(),
             'direccion'           => $this->string()->notNull(),
             'telefono'            => $this->string()->notNull(),
             'celular'             => $this->string()->notNull(),
-            'email'               => $this->string()->notNull(),
+            'email'               => $this->string()->notNull()->unique(),
             'creado_por'          => $this->integer()->notNull(),
             'creado_en'           => $this->timestamp()->notNull(),
             'activo'              => $this->integer()->notNull()->defaultValue(1)
