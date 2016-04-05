cf9e166dfb69c1de3af7da625aba921b3a231888
Alcides <alcidesrodriguezreyes@gmail.com>
2016-04-04 20:43:04 -0500

    create table estudiante

---
+++ yii/console/migrations/m160405_010504_create_student_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160405_010504_create_student_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('estudiante', [
+            'idestudiante'        => $this->primaryKey(),
+            'tipo_identificacion' => $this->integer()->notNull(),
+            'identificacion'      => $this->string()->notNull(),
+            'nombres'             => $this->string()->notNull(),
+            'apellidos'           => $this->string()->notNull(),
+            'direccion'           => $this->string()->notNull(),
+            'telefono'            => $this->string()->notNull(),
+            'celular'             => $this->string()->notNull(),
+            'email'               => $this->string()->notNull(),
+            'creado_por'          => $this->integer()->notNull(),
+            'creado_en'           => $this->timestamp()->notNull(),
+            'activo'              => $this->integer()->notNull()->defaultValue(1)
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('estudiante');
+    }
+}
