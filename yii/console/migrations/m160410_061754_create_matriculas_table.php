be715d2314fbf359f1e44ed089105f6938c0f8f9
Alcides <alcidesrodriguezreyes@gmail.com>
2016-04-10 10:45:24 -0500

    matriculas table

--- yii/console/migrations/m160410_061754_create_matriculas_table.php
+++ yii/console/migrations/m160410_061754_create_matriculas_table.php
@@ -7,10 +7,16 @@ class m160410_061754_create_matriculas_table extends Migration
     public function up()
     {
         $this->createTable('matriculas', [
-            'idmatricula' => $this->primaryKey(),
-            'fecha_matricula' => $this->timestamp()->notNull(),
-            'valor' => $this->docuble()->notNull(),
-            'estudiante_idestudiante' = $this->integer()->notNull(),
+            'idmatricula'             => $this->primaryKey(),
+            'fecha_matricula'         => $this->timestamp()->notNull(),
+            'valor'                   => $this->double()->notNull(),
+            'estudiante_idestudiante' => $this->integer()->notNull(),
+            'grupo'                   => $this->string(),
+            'estado'                  => $this->integer()->notNull()->defaultValue(1),
+            'periodo_idperiodo'       => $this->integer()->notNull(),
+            'creado_por'              => $this->integer()->notNull(),
+            'creado_en'               => $this->timestamp()->notNull(),
+            'activo'                  => $this->integer()->notNull()->defaultValue(1)
         ]);
     }
 
