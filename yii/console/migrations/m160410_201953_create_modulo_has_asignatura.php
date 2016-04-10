f0ae913fdbcc177ab4e7c3902b69bb9a970c5f67
Alcides <alcidesrodriguezreyes@gmail.com>
2016-04-10 15:48:55 -0500

    modulo_has_asignatura

--- yii/console/migrations/m160410_201953_create_modulo_has_asignatura.php
+++ yii/console/migrations/m160410_201953_create_modulo_has_asignatura.php
@@ -4,16 +4,36 @@ use yii\db\Migration;
 
 class m160410_201953_create_modulo_has_asignatura extends Migration
 {
-    public function up()
+    public function safeUp()
     {
         $this->createTable('modulo_has_asignatura', [
           'modulo_idmodulo' => $this->integer()->notNull(),
           'asignatura_idasignatura' => $this->integer()->notNull()
         ]);
+        $this->addForeignKey(
+          'fk_modulo_has_asignatura_modulo1',
+          'modulo_has_asignatura',
+          'modulo_idmodulo',
+          'modulo',
+          'idmodulo',
+          'NO ACTION',
+          'NO ACTION'
+        );
+        $this->addForeignKey(
+          'fk_modulo_has_asignatura_asignatura1',
+          'modulo_has_asignatura',
+          'asignatura_idasignatura',
+          'asignatura',
+          'idasignatura',
+          'NO ACTION',
+          'NO ACTION'
+        );
     }
 
-    public function down()
+    public function safeDown()
     {
-        $this->dropTable('modulo_has_asignatura');
+      $this->dropForeignKey('fk_modulo_has_asignatura_modulo1', 'modulo_has_asignatura');
+      $this->dropForeignKey('fk_modulo_has_asignatura_asignatura1', 'modulo_has_asignatura');
+      $this->dropTable('modulo_has_asignatura');
     }
 }
